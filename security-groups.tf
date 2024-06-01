caad0fe909e3c4ae3ef058bdf95b4adada09dddf
praveenchaganti20 <158231388+praveenchaganti20@users.noreply.github.com>
2024-06-01 09:48:15 -0500

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -6,16 +6,12 @@ resource "aws_security_group" "all_worker_mgmt" {
 
 resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
   description       = "allow inbound traffic from eks"
-  from_port         = 0
+  from_port         = 443
   protocol          = "-1"
-  to_port           = 0
+  to_port           = 443
   security_group_id = aws_security_group.all_worker_mgmt.id
   type              = "ingress"
-  cidr_blocks = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16",
-  ]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "all_worker_mgmt_egress" {
